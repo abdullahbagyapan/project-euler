7e6e75dfcd9ea7b66f3b25a6dc9b905545096360
Abdullah Bagyapan <229599340+abdullahbagyapan@users.noreply.github.com>
2025-08-31 20:47:53 +0300

    feat: add solution for problem 1

---
+++ #1-multiples-of-3-or-5/arithmetic-progression.py
@@ -0,0 +1,19 @@
+k_3 = (999 - 3) / 3 + 1
+sum_of_set_3 = (k_3 * (999 + 3)) / 2
+
+k_5 = (995 - 5) / 5 + 1
+sum_of_set_5 = (k_5 * (995 + 5)) / 2
+
+k_15 = (990 - 15) / 15 + 1
+sum_of_set_15 = (k_15 * (990 + 15)) / 2
+
+
+sum = sum_of_set_3 + sum_of_set_5 - sum_of_set_15
+
+print(int(sum)) # Output should be 233168
+
+
+# Relative links:
+# https://en.wikipedia.org/wiki/Arithmetic_progression
+# https://en.wikipedia.org/wiki/Set_(mathematics)
+
